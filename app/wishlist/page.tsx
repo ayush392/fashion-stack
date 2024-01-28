ecfa46250a496213fafaf0ea6bb377498078dcec
ayush392 <ayushgoyal.0004@gmail.com>
2024-01-29 02:50:17 +0530

    feat: Add Wishlist Page and Wishlist Controller

---
+++ app/wishlist/page.tsx
@@ -0,0 +1,83 @@
+"use client";
+import { useEffect, useState } from "react";
+import Card from "@/components/cards/Card";
+import { addToCartFromWishlist, getWishlist } from "@/utils/wishlistController";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+
+function Wishlist() {
+  const [products, setProducts] = useState([]);
+  const router = useRouter();
+
+  async function fetchData() {
+    const res = await getWishlist();
+    console.log(res);
+    setProducts(res);
+  }
+
+  async function handleMove(productId: string, size: string) {
+    try {
+      const res = await addToCartFromWishlist(productId, size);
+      setProducts(res);
+      alert("Product moved to cart");
+    } catch (error: any) {
+      alert(error.message);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className="max-w-screen-xl m-auto">
+      <h1 className="text-xl font-bold my-3">
+        My Wishlist
+        <span className=" font-medium text-lg">{` (${products.length} items)`}</span>
+      </h1>
+
+      <div className="flex flex-wrap justify-between">
+        {products.length > 0 &&
+          products.map((data: any, i: number) => {
+            return (
+              <div
+                key={i}
+                className=" w-fit max-w-[49%] mb-2 sm:max-w-[210px] border cursor-pointer lg:m-4 lg:ml-0"
+              >
+                <Image
+                  className=" aspect-[3/4]"
+                  src={data.product.imageUrl}
+                  alt={data.product.title}
+                  height={280}
+                  width={210}
+                  onClick={() => router.push(`/products/${data.product._id}`)}
+                />
+                <div className="px-2 py-1">
+                  <p className=" text-xs text-slate-700 truncate my-2">
+                    {data.product.title}
+                  </p>
+                  <div className="flex gap-1 text-xs lg:text-sm items-baseline my-2">
+                    <p className=" text-sm lg:text-base font-bold">{`Rs. ${data.product.price}`}</p>
+                    <p className=" line-through text-gray-500">
+                      {data.product.mrp}{" "}
+                    </p>
+                    <p className="text-green-500">{`(-${data.product.discount}%)`}</p>
+                  </div>
+                </div>
+                <button
+                  className="w-full border-t py-2 text-center font-semibold text-orange-600"
+                  onClick={() =>
+                    handleMove(data.product._id, data.product.sizes[0])
+                  }
+                >
+                  Move to Cart
+                </button>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+}
+
+export default Wishlist;
---
+++ utils/wishlistController.ts
@@ -0,0 +1,82 @@
+"use server";
+import connect from "@/config/db";
+import productModel from "@/models/product.model";
+import userModel from "@/models/user.model";
+import accountModel from "@/models/account.model";
+import { getUserId } from "./cartController";
+import { addToCart } from "./cartController";
+
+export async function addToWishlist(productId: any) {
+  try {
+    await connect();
+    const userId = await getUserId();
+    if (!userId || !productId) {
+      throw new Error("Please login");
+    }
+    const data = await accountModel.findOne({ user: userId });
+    data.wishlist.push({ product: productId });
+    await data.save();
+    return "item added to wishlist";
+  } catch (error) {
+    console.log(error, "utils-addtoWishlist");
+    return (error as Error).message;
+  }
+}
+
+export async function getWishlist() {
+  try {
+    await connect();
+    const userId = await getUserId();
+    if (!userId) {
+      throw new Error("Please login");
+    }
+
+    const res = await accountModel
+      .findOne({ user: userId })
+      .populate({
+        path: "wishlist.product",
+        select: "imageUrl title price mrp discount sizes",
+      })
+      .select("wishlist");
+    console.log(res.wishlist, "res");
+    // return res.wishlist;
+    return await JSON.parse(JSON.stringify(res.wishlist));
+  } catch (error) {
+    return (error as Error).message;
+  }
+}
+
+export async function removeFromWishlist(productId: any) {
+  try {
+    await connect();
+    const userId = await getUserId();
+    if (!userId || !productId) {
+      throw new Error("Please login");
+    }
+    const update = await accountModel
+      .findOneAndUpdate(
+        { user: userId },
+        { $pull: { wishlist: { product: productId } } },
+        { new: true }
+      )
+      .populate({
+        path: "wishlist.product",
+        select: "imageUrl title price mrp discount sizes",
+      });
+    console.log(update.wishlist, "update");
+    return await JSON.parse(JSON.stringify(update.wishlist));
+  } catch (error) {
+    console.log(error, "utils-removeFromWishlist");
+    return (error as Error).message;
+  }
+}
+
+export async function addToCartFromWishlist(productId: any, size: string) {
+  try {
+    await addToCart(productId, 1, size);
+    const res = await removeFromWishlist(productId);
+    return res;
+  } catch (error) {
+    return (error as Error).message;
+  }
+}
