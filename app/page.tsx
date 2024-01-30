13b63e3dcd4eec8e7770ea08a207d505b9331e17
ayush392 <ayushgoyal.0004@gmail.com>
2024-01-30 20:12:16 +0530

    feat: add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,82 +1,43 @@
-"use client";
-import { useEffect, useState } from "react";
+import Image from "next/image";
+import React from "react";
+import Link from "next/link";
 
-export default function Home() {
-  const [user, setUser] = useState({
-    email: "",
-    password: "",
-    fullName: "",
-    state: "",
-    country: "",
-    username: "",
-  });
-
-  const handleLogout = () => {
-    localStorage.removeItem("token");
-    window.location.href = "/login";
-  };
-
-  const getCredentials = async () => {
-    try {
-      const res = await fetch("api/dashboard", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          // Authorisation: "Bearer " + localStorage.getItem("token"),
-        },
-        credentials: "include",
-      });
-      const json = await res.json();
-      if (json.status === 200) {
-        setUser(json.user);
-        // console.log(json.user);
-      } else {
-        window.location.href = "/login";
-      }
-    } catch (error: any) {
-      alert(error.message);
-    }
-  };
-
-  useEffect(() => {
-    getCredentials();
-  }, []);
-
+function Home() {
   return (
-    <>
-      {user.email === "" ? (
-        <div>Loading...</div>
-      ) : (
-        <div>
-          <div className="container min-h-dvh flex justify-center items-center">
-            <div className="max-w-md w-full p-6 text-center border shadow-lg rounded-lg">
-              <h1 className=" text-3xl font-bold">{`Welcome ${user.fullName}!`}</h1>
-              <h2 className="text-2xl text-gray-600 font-semibold">
-                {`@${user.username}`}
-              </h2>
-              <div className="mt-5 text-lg">
-                <p className="text-gray-600">
-                  <span className="font-semibold">Email:</span> {user.email}
-                </p>
-                {user?.state && (
-                  <p className="text-gray-600">
-                    <span className="font-semibold">State:</span> {user.state}
-                  </p>
-                )}
-                <p className="text-gray-600">
-                  <span className="font-semibold">Country:</span> {user.country}
-                </p>
-              </div>
-            </div>
-          </div>
-          <button
-            className=" absolute right-4 top-4 bg-red-500 text-white px-4 py-2 rounded-md"
-            onClick={handleLogout}
-          >
-            Logout
-          </button>
-        </div>
-      )}
-    </>
+    <div className=" min-h-svh -mt-4">
+      <Link href="/products">
+        <Image
+          className="w-full h-auto"
+          src="/images/banner.jpg"
+          alt="banner"
+          width={1800}
+          height={800}
+        />
+      </Link>
+
+      <div className="flex flex-col md:flex-row max-w-7xl mx-auto gap-5 mt-4 md:mt-8">
+        <Link href="/products?gender=Men">
+          <Image
+            className="w-[900px] h-auto"
+            src="/images/lhs.jpg"
+            alt="banner"
+            width={900}
+            height={400}
+          />
+        </Link>
+
+        <Link href="/products?gender=Women">
+          <Image
+            className="w-[900px] h-auto"
+            src="/images/rhs.webp"
+            alt="banner"
+            width={900}
+            height={400}
+          />
+        </Link>
+      </div>
+    </div>
   );
 }
+
+export default Home;
